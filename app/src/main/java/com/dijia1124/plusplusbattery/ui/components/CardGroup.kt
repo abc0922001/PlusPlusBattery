2b88842d685a8db07bd0c541d14288bae970283e
dijia1124 <zrh19981123@gmail.com>
2025-10-16 04:03:13 +1100

    feat(ui): implement a card group title component & add it into the card group

--- app/src/main/java/com/dijia1124/plusplusbattery/ui/components/CardGroup.kt
+++ app/src/main/java/com/dijia1124/plusplusbattery/ui/components/CardGroup.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,19 +14,26 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun CardGroup(
     modifier: Modifier = Modifier,
+    title: (@Composable () -> Unit)? = null,
     content: List<@Composable () -> Unit>
 ) {
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(2.dp)
+        modifier = modifier
     ) {
-        content.forEachIndexed { index, item ->
-            Box(
-                modifier = Modifier
-                    .clip(getListItemShape(index, content.size))
-                    .background(MaterialTheme.colorScheme.surface)
-            ) {
-                item()
+        title?.let {
+            Box(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp)) {
+                it()
+            }
+        }
+        Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+            content.forEachIndexed { index, item ->
+                Box(
+                    modifier = Modifier
+                        .clip(getListItemShape(index, content.size))
+                        .background(MaterialTheme.colorScheme.surface)
+                ) {
+                    item()
+                }
             }
         }
     }
